26cc14e4f302049cfbf65bbbb211a7866ad0f136
tdimnet <thomas.dimnet@gmail.com>
2018-04-18 20:34:11 +0200

    Add all the questions data needed.

--- TrueFalseStarter/questions.data.swift
+++ TrueFalseStarter/questions.data.swift
@@ -15,3 +15,95 @@ let firstQuestion = Question(
         Answer(answer: "Andrew Jackson", isCorrect: false),
     ]
 )
+
+let secondQuestion = Question(
+    question: "Which of the following countries has the most residents?",
+    answers: [
+        Answer(answer: "Nigeria", isCorrect: true),
+        Answer(answer: "Russia", isCorrect: false),
+        Answer(answer: "Iran", isCorrect: false),
+        Answer(answer: "Vietnam", isCorrect: false),
+        ]
+)
+
+let thirdQuestion = Question(
+    question: "In what year was the United Nations founded?",
+    answers: [
+        Answer(answer: "1918", isCorrect: false),
+        Answer(answer: "1919", isCorrect: false),
+        Answer(answer: "1945", isCorrect: true),
+        Answer(answer: "1954", isCorrect: false),
+        ]
+)
+
+let fourthQuestion = Question(
+    question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?",
+    answers: [
+        Answer(answer: "Paris", isCorrect: false),
+        Answer(answer: "Washington D.C.", isCorrect: false),
+        Answer(answer: "New York City", isCorrect: true),
+        Answer(answer: "Boston", isCorrect: false),
+        ]
+)
+
+let fifthQuestion = Question(
+    question: "Which nation produces the most oil?",
+    answers: [
+        Answer(answer: "Iran", isCorrect: false),
+        Answer(answer: "Iraq", isCorrect: false),
+        Answer(answer: "Brazil", isCorrect: false),
+        Answer(answer: "Canada", isCorrect: true),
+        ]
+)
+
+let sixthQuestion = Question(
+    question: "Which country has most recently won consecutive World Cups in Soccer?",
+    answers: [
+        Answer(answer: "Italy", isCorrect: false),
+        Answer(answer: "Brazil", isCorrect: true),
+        Answer(answer: "Argetina", isCorrect: false),
+        Answer(answer: "Spain", isCorrect: false),
+        ]
+)
+
+let seventhQuestion = Question(
+    question: "Which of the following rivers is longest?",
+    answers: [
+        Answer(answer: "Yangtze", isCorrect: false),
+        Answer(answer: "Mississippi", isCorrect: true),
+        Answer(answer: "Congo", isCorrect: false),
+        Answer(answer: "Mekong", isCorrect: false),
+        ]
+)
+
+let eighthQuestion = Question(
+    question: "Which city is the oldest?",
+    answers: [
+        Answer(answer: "Mexico City", isCorrect: true),
+        Answer(answer: "Cape Town", isCorrect: false),
+        Answer(answer: "San Juan", isCorrect: false),
+        Answer(answer: "Sydney", isCorrect: false),
+        ]
+)
+
+let ninthQuestion = Question(
+    question: "Which country was the first to allow women to vote in national elections?",
+    answers: [
+        Answer(answer: "Poland", isCorrect: true),
+        Answer(answer: "United States", isCorrect: false),
+        Answer(answer: "Sweden", isCorrect: false),
+        Answer(answer: "Senegal", isCorrect: false),
+        ]
+)
+
+let tenthQuestion = Question(
+    question: "Which of these countries won the most medals in the 2012 Summer Games?",
+    answers: [
+        Answer(answer: "France", isCorrect: false),
+        Answer(answer: "Germany", isCorrect: false),
+        Answer(answer: "Japan", isCorrect: false),
+        Answer(answer: "Great Britian", isCorrect: true),
+        ]
+)
+
+
